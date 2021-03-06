b8bcb2335cc44362726c9a52bc1b018fbeb4d3b9
Manuel Duarte <manuelduarteh@gmail.com>
2021-03-05 20:30:31 -0700

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+            <h1>
+                Ejercio1
+            </h1>
+            <button>Ir</button>
+        </div>
+        <div>
+            <h1>
+                Ejercio2
+            </h1>
+            <button>Ir</button>
+        </div>
+        <div>
+            <h1>
+                Ejercio3
+            </h1>
+            <button>Ir</button>
+        </div>
+        <div>
+            <h1>
+                Ejercio4
+            </h1>
+            <button>Ir</button>
+        </div>
+        <div>
+            <h1>
+                Ejercio5
+            </h1>
+            <button>Ir</button>
+        </div>
+    </div>
+  );
+}
+
+export default App;
